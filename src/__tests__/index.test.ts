f06ab73992d48f9835449a6c83d95ae2ee09345b
Mihael Konjevic <konjevic@gmail.com>
2022-07-29 21:56:27 +0200

    Initial fleshing out of decomposition feature

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,28 +1,48 @@
-import { describe, expect, it } from "vitest";
-import { foo, bar } from "../index";
+import { describe, expect, it } from 'vitest';
+import { decomposeOne, schema } from '../index';
 
-describe("foo()", () => {
-  describe("given two positive integers", () => {
-    const first = 1;
-    const second = 2;
-
-    describe("when called", () => {
-      it("returns the sum of them", () => {
-        expect(foo(first, second)).toEqual(3);
-      });
-    });
+describe('schema()', () => {
+  it('should build schema with one attribute', () => {
+    const s = schema('parent_id').withMapping('parent_id', 'id');
+    const decomposed = s.decomposeAll([{ parent_id: 1 }]);
+    expect(decomposed).toEqual([{ id: 1 }]);
+  });
+  it('should build schema with multiple attributes', () => {
+    const s = schema('parent_id')
+      .withMapping('parent_id', 'id')
+      .withMapping('parent_val', 'val');
+    const decomposed = s.decomposeAll([{ parent_id: 1, parent_val: 'p1' }]);
+    expect(decomposed).toEqual([{ id: 1, val: 'p1' }]);
   });
 });
 
-describe("bar()", () => {
-  describe("given two positive integers", () => {
-    const first = 2;
-    const second = 1;
-
-    describe("when called", () => {
-      it("returns the subtraction of them", () => {
-        expect(bar(first, second)).toEqual(1);
-      });
+/*describe('decompose()', () => {
+  it('should collapse simple tree structures', () => {
+    const data = [
+      { parent_id: 1, parent_val: 'p1', children_id: 11, children_val: 'c1' },
+      { parent_id: 1, parent_val: 'p1', children_id: 12, children_val: 'c2' },
+    ];
+    const decomposed = decompose(
+      {
+        pk: 'parent_id',
+        schema: {
+          id: 'parent_id',
+          val: 'parent_val',
+          children: {
+            pk: 'children_id',
+            schema: { id: 'children_id', val: 'children_val' },
+          },
+        },
+      },
+      data
+    );
+    expect(decomposed).toEqual({
+      id: 1,
+      val: 'p1',
+      children: [
+        { id: 11, val: 'c1' },
+        { id: 12, val: 'c2' },
+      ],
     });
   });
-});
+});*/
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,106 @@
-export function foo(a: number, b: number): number {
-  return a + b;
+type Pk = string[];
+
+type IHasDecompose<S extends string, T extends string> = {
+  pk: Pk;
+  source: S;
+  target: T;
+  mappings: { [k in T]: S };
+};
+
+type ValueOf<T> = T[keyof T];
+
+interface IMappable {
+  pk: Pk;
+  decompose: (item: unknown) => unknown;
 }
 
-export function bar(a: number, b: number): number {
-  return a - b;
+class Schema {
+  constructor(readonly pk: Pk) {}
+  withMapping<S1 extends string, T1 extends string>(source: S1, target: T1) {
+    return new RootMapping(this.pk, source, target);
+  }
 }
+
+class RootMapping<S extends string, T extends string>
+  implements IHasDecompose<S, T>
+{
+  readonly pk: Pk;
+  readonly source: S;
+  readonly target: T;
+  readonly mappings: { [k in T]: S };
+  constructor(pk: Pk, source: S, target: T) {
+    this.pk = pk;
+    this.source = source;
+    this.target = target;
+    this.mappings = { [target]: source } as { [k in T]: S };
+  }
+  decompose<I extends Record<S, unknown>>(item: I): { [k in T]: I[S] } {
+    return { [this.target]: item[this.source] } as { [k in T]: I[S] };
+  }
+  decomposeAll<I extends Record<S, unknown>>(items: I[]) {
+    return items.map((i) => this.decompose(i));
+  }
+  withMapping<S1 extends string, T1 extends string>(source: S1, target: T1) {
+    return new Mapping(this, source, target);
+  }
+}
+
+class Mapping<
+  P extends IHasDecompose<string, string>,
+  S extends string,
+  T extends string
+> implements IHasDecompose<S, T>
+{
+  readonly pk: Pk;
+  readonly source: S;
+  readonly target: T;
+  readonly mappings: P['mappings'] & { [k in T]: S };
+
+  constructor(prev: P, source: S, target: T) {
+    this.pk = prev.pk;
+    this.source = source;
+    this.target = target;
+    this.mappings = { ...prev.mappings, [target]: source } as P['mappings'] & {
+      [k in T]: S;
+    };
+  }
+
+  decompose<I extends Record<ValueOf<this['mappings']>, unknown>>(item: I) {
+    const val: Record<string, unknown> = {};
+    Object.entries(this.mappings).forEach(([k, v]) => {
+      val[k] = item[v as ValueOf<this['mappings']>];
+    });
+    return val as {
+      [Property in keyof this['mappings']]: I[this['mappings'][Property]];
+    };
+  }
+
+  decomposeAll<I extends Record<ValueOf<this['mappings']>, unknown>>(
+    items: I[]
+  ) {
+    return items.map((i) => this.decompose(i));
+  }
+
+  withMapping<S1 extends string, T1 extends string>(source: S1, target: T1) {
+    return new Mapping(this, source, target);
+  }
+}
+
+export function schema(...pks: Pk) {
+  return new Schema(pks);
+}
+
+const s = schema('foo')
+  .withMapping('parent_val', 'val')
+  .withMapping('parent_id', 'id');
+
+const b = s.decomposeAll([
+  { parent_val: 'foo', parent_id: 1 },
+  { parent_val: 1, parent_id: 'foo' },
+]);
+
+/*const r = new RootMapping('parent_id', 'id')
+  .withMapping('parent_val', 'val')
+  .withMapping('parent_foo', 'foo');
+
+const a = r.decompose({ parent_id: 1, parent_val: 'foo', parent_foo: 'foo' });*/
